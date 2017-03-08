fbebec7857276d149ff03f91d2a51d95a91368fd
Ola-Alkhateeb <ola.93.alkhateeb@gmail.com>
2017-03-08 17:44:04 +0200

    adding models controllers but still need to be tested

--- server/db/faq/quastionController.js
+++ server/db/faq/quastionController.js
@@ -1,1 +1,31 @@
 var quastion = require("./quastionModel.js");
+
+module.exports ={
+	getAllQuastions : function (req, res) {
+	  quastion.find().exec(function (err, data) {
+	    if(err){
+		  res.status(500).send('err');
+		}else{
+		  res.json(data)
+		}
+		});
+	},
+	insertQuastion : function (req, res) {
+	  var newQuastion = new quastion(req.body);  
+      quastion.save(function (err, newQuastion) {  
+        if (err) {
+          res.send(err);
+        }
+        res.send(newQuastion);
+        });  
+	},
+	getOne : function (req,res) {
+	  quastion.findById(req.params.id, function (err, data) {  
+        if (err) {
+          res.send(err)
+        }else{
+          res.json(data)
+        } 
+        })
+    }
+}
--- server/db/projects/projectsController.js
+++ server/db/projects/projectsController.js
@@ -1,1 +1,22 @@
 var projects = require("./projectsModel.js");
+
+module.exports ={
+	getAllProjects : function (req, res) {
+	  projects.find().exec(function (err, data) {
+	    if(err){
+		  res.status(500).send('err');
+		}else{
+		  res.json(data)
+		}
+		});
+	},
+	insertProject : function (req, res) {
+	  var newProject= new projects(req.body);  
+      projects.save(function (err, newProject) {  
+        if (err) {
+          res.send(err);
+        }
+        res.send(newProject);
+        });  
+	}
+}
