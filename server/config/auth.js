ecf9cfa734dfac03834fb684020bc1e859d5a813
semo94 <saleem.bakri94@gmail.com>
2017-03-11 20:05:58 +0200

    (feat) edit authConfig

--- server/config/auth.js
+++ server/config/auth.js
@@ -1,7 +1,7 @@
 module.exports = {
   'gitHubAuth' : {
-    'clientID' : 'ff4babb665537f62773a',
-    'clientSecret' : '95785083bb89b7a014088c5c56e98ceb3c983a75',
+    'clientID' : '7914b751f7518ffd6520',
+    'clientSecret' : '14b8b9d243a551b7207f7216fa3169a1d1ad374c',
     'callbackURL' : 'http://localhost:5000/auth/github/callback'
   }
 }
