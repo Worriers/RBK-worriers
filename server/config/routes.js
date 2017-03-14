36520d7fdca8e54a57f071f4d7b972a3622643be
semo94 <saleem.bakri94@gmail.com>
2017-03-14 20:29:16 +0200

    (test)user spec done

--- server/config/routes.js
+++ server/config/routes.js
@@ -42,6 +42,7 @@ app.get('/api/isLogged',utils.isLogged);
 
 //getting all profiles and editing profiles 
 app.get('/api/profile' , userController.getAllUsers);
+app.get('/api/profile:username',userController.getOneUser);
 app.post('/api/profile', userController.updateAccount);
 app.delete('/api/profile', userController.deleteUser);
 // getting and adding achievments 
