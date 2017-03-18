10fd6c77b1851d9129a978f5a9be56f8f40bed1f
montaserRahmani <montaser.rah@gmail.com>
2017-03-18 10:40:46 +0200

    Added webshot feature to insert project route, edited main page to show images

--- server - Copy/config/auth.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  'gitHubAuth' : {
-    'clientID' : process.env.GITHUB_CLIENT_ID,
-    'clientSecret' : process.env.GITHUB_SECRET,
-    'callbackURL' : process.env.GITHUB_CALLBACK_URL
-  }
-}
