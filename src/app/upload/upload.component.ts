7151f29c8ca69cd133b166e1487425489297340f
semo94 <saleem.bakri94@gmail.com>
2017-03-19 13:48:44 +0200

    (feat) delete images admin template

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FileUploader } from 'ng2-file-upload';
+import { GalleryService } from '../shared/gallery.service';
 
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
-  styleUrls: ['./upload.component.css']
+  styleUrls: ['./upload.component.css'],
+  providers: [GalleryService]
 })
 export class UploadComponent implements OnInit {
   public uploader:FileUploader = new FileUploader({url:'http://127.0.0.1:5000/api/gallery'});
@@ -12,9 +14,18 @@ export class UploadComponent implements OnInit {
   public fileOverBase(e:any):void {
     this.hasBaseDropZoneOver = e;
   }
-  constructor() { }
-
+  constructor(private galleryService : GalleryService) { }
+  images : any[] = [];
   ngOnInit() {
+    this.getImages();
+  }
+
+  getImages() {
+    this.galleryService.getImages().then((data) => {
+        this.images = data;
+        console.log(data);
+    });
+
   }
 
 }
