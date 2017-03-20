25dfeef57bea599083f1072c84bbb521a9ed3344
DaniaHamdan <dandonzh@gmail.com>
2017-03-20 17:45:15 +0200

    user

--- src/app/add-project/add-project.component.ts
+++ src/app/add-project/add-project.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AddProjectService } from '../shared/addproject.service';
+import  {GradsService} from '../shared/grads.service';
+
 @Component({
   selector: 'app-add-project',
   templateUrl: './add-project.component.html',
@@ -8,15 +10,20 @@ import { AddProjectService } from '../shared/addproject.service';
 })
 export class AddProjectComponent implements OnInit {
   drob : Object[]=[];
-  constructor(private addProjectService: AddProjectService) { }
+  num : string ;
+  constructor(private addProjectService: AddProjectService, private gradesprivate: GradsService ) { }
 
   ngOnInit() {
-    this.drob.push("ola","dania")
+    this.num=localStorage.getItem("rbk.cohort")
+    this.getgrads()
   }
 
 addProject(title,url,gitHubLink,teamMembers,img){
     console.log(title,url,gitHubLink,teamMembers)
     this.addProjectService.insertProject({title : title , url : url , gitHubLink : gitHubLink , teamMembers : teamMembers }) ;
+    }
+    getgrads(){
+      this.gradesprivate.getGradList(this.num).then((data) => this.drob = data );
     }
 
 }
