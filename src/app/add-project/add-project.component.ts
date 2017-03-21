fb6f2cea243451d53b15ebde204641f6716df19b
DaniaHamdan <dandonzh@gmail.com>
2017-03-21 16:38:32 +0200

    edit add project

--- src/app/add-project/add-project.component.ts
+++ src/app/add-project/add-project.component.ts
@@ -1,25 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewContainerRef } from '@angular/core';
+import { Modal } from 'angular2-modal/plugins/bootstrap';
+import { Overlay } from 'angular2-modal';
 import { AddProjectService } from '../shared/addproject.service';
 import  {GradsService} from '../shared/grads.service';
+import { Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-add-project',
   templateUrl: './add-project.component.html',
   styleUrls: ['./add-project.component.css'],
-  providers: [AddProjectService]
+  providers: [AddProjectService, Modal, Overlay]
 })
 export class AddProjectComponent implements OnInit {
   drob : Object[] = [];
   num : String ;
   flag : Number;
-  constructor(private addProjectService: AddProjectService, private gradeService: GradsService ) { }
+  url1 :  Object[]=[];
+  constructor(private addProjectService: AddProjectService, private gradeService: GradsService , overlay: Overlay, vcRef: ViewContainerRef, public modal: Modal , private router : Router) { 
+  overlay.defaultViewContainer = vcRef;
+}
 
   ngOnInit() {
     this.getgrads()
   }
 
 addProject(title,url,gitHubLink,teamMembers,img){
-    console.log(title,url,gitHubLink,teamMembers)
     for(var i=0;i<teamMembers.length;i++){
       if(teamMembers.indexOf(teamMembers[i])!==i){
         teamMembers.splice(i,1)
@@ -28,14 +34,21 @@ addProject(title,url,gitHubLink,teamMembers,img){
         teamMembers.splice(teamMembers.indexOf(undefined),1)
       }
     }
-
-     console.log(arguments.length,teamMembers)
+    this.url1=url.split(":")
      this.addProjectService.insertProject({title : title , url : url , gitHubLink : gitHubLink , teamMembers : teamMembers }).then((data)=> {
-       if(data.status===201){
-         this.flag=201;
-       }
-       console.log(this.flag)
+      if(this.url1.includes("https")||this.url1.includes("http")){
+         this.modal.alert()
+        .title('Admin!')
+        .body('Add project is completed successful but wait admin to approve yor reqyest')
+        .open();
+      }else {
+         this.modal.alert()
+        .title('Admin!')
+        .body('pleas check your input url project')
+        .open();
+      }
      }) ;
+      this.router.navigate([('/')]);
      }
       
     getgrads(){
