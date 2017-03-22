0ec755d3314de241a17a20a23353a185a930f5a9
Ola-Alkhateeb <ola.93.alkhateeb@gmail.com>
2017-03-22 15:24:42 +0200

    fixing

--- src/app/qa/qa.component.ts
+++ src/app/qa/qa.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewContainerRef } from '@angular/core';
 import { QaService } from '../shared/qa.service';
 import { Modal } from 'angular2-modal/plugins/bootstrap';
 import { Overlay } from 'angular2-modal';
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-qa',
@@ -16,51 +17,50 @@ export class QaComponent implements OnInit {
 	myVar : boolean = false ; 
 	newEntry: any = {}; 
 	newComment : any = {} ;
-  constructor(private qaSevices : QaService,vcRef: ViewContainerRef, overlay: Overlay, public modal: Modal) {
+  constructor(private qaSevices : QaService,vcRef: ViewContainerRef, overlay: Overlay, public modal: Modal , private router : Router) {
     overlay.defaultViewContainer = vcRef }
 
 
-  ngOnInit() {
-  	this.getQuestions();
-  }
+    ngOnInit() {
+      this.getQuestions();
+    }
 
-  getQuestions() {
-  	this.qaSevices.getQuestions().then((data) => {
-  		data.forEach(q => {
-  			q.show = false;
-  		})
-  		this.q = data;
-  	});
-  }
+    getQuestions() {
+      this.qaSevices.getQuestions().then((data) => {
+        data.forEach(q => {
+          q.show = false;
+        })
+        this.q = data;
+      });
+    }
 
-  addQ(name , q ){
-  	if(name === undefined){
-  		name = "visitor"
-  	}
-  	this.qaSevices.addQuestion({name : name , text : q}) ;
-  	this.newEntry = {};
-  	this.getQuestions();
-    this.modal.alert() 
+    addQ(name , q ){
+      if(name === undefined){
+        name = "visitor"
+      }
+      this.qaSevices.addQuestion({name : name , text : q}) ;
+      this.newEntry = {};
+      this.getQuestions();
+      this.modal.alert() 
       .title('Thanks for asking')
       .body("your question will be answered as soon as possible :)")
       .open();
-  }
+    }
 
-  addC(qText , c) {
-    if(localStorage.getItem("rbk.isLogged")){
-      this.router.navigate([('/')]);
-    }){
- 	this.qaSevices.addComment({qText :qText , text : c });
- 	setTimeout(() => this.getQuestions() , 500);
- 	this.newComment = {} ;
- } else {
-   this.modal.alert()
-   .title('Ooopss')
-   .body("Make sure you're logged in before adding this comment")
-   .open()
-     this.newComment = {} ;
- }
-  }
-
+    addC(qText , c) {
+      if(localStorage.getItem("rbk.isLogged")){
+        this.router.navigate([('/')]);
+        this.qaSevices.addComment({qText :qText , text : c });
+        setTimeout(() => this.getQuestions() , 500);
+        this.newComment = {} ;
+      } else {
+        this.modal.alert()
+        .title('Ooopss')
+        .body("Make sure you're logged in before adding this comment")
+        .open()
+        this.newComment = {} ;
+      }
+    }
 
 }
+
